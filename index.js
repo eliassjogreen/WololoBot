d1fa9ceba2a6299ebd90a271b1770ed2b338713e
Elias <eliassjogreen1@gmail.com>
2017-11-06 15:55:46 +0100

    Fixed stuff for heroku!

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
 const r = new Snoowrap({
-    userAgent: 'reddit-bot-example-node',
+    userAgent: 'reddit-bot-aoe2-wololo',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
@@ -19,9 +19,12 @@ const streamOpts = {
 
 const comments = client.CommentStream(streamOpts);
 
+console.log("Listening for Wololo's");
 comments.on('comment', (comment) => {
     if (comment.body.toLowerCase().includes('wololo') && comment.author.name !== "WololoBot") {
-        console.log(comment);
+        console.log("Wololo detected: ");
+        console.log("   Comment: " + comment.body);
+        console.log("   Author:  " + comment.author.name);
         comment.reply('[Wololo](http://wololo.se/)\n---\n^(I am a bot | ALL HAIL KING OF THE LOSERS! My creator u/E-Mouse)');
     }
 });
